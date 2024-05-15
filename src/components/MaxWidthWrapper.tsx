fb676c92eb63dd84d55bbfe3e7219606bc901853
Vlas Dvorovyi <vlas20421@gmail.com>
2024-05-15 08:40:07 +0300

    feat: created project & add packages

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/utils/cn";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className={cn("h-full mx-auto w-full", className)}>{children}</div>
+  );
+};
+
+export default MaxWidthWrapper;
